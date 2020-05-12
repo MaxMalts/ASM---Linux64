3eadf3410ea46a84ae58878ff6becdf6671aa352
MaxMalts <max-kurt@bk.ru>
2020-05-12 16:19:45 +0300

    Programs written so fat

---
+++ Crack/Crack/Crack/Crack.cpp
@@ -0,0 +1,187 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+//#include <unistd.h>
+
+
+
+struct Change {
+	int position;
+	char origVal;
+	char newVal;
+};
+
+struct Change changes[] = {
+	{0xc8, 0x40, 0x60},
+	{0xc7, 0x00, 0x02},
+	{0xc6, 0xf2, 0x2f},
+
+	{0xcf, 0x0d, 0x01},
+
+	{0x1a9, 0x60, 0x60},
+	{0x1a8, 0x02, 0x02},
+	{0x1a7, 0x47, 0x2e},
+};
+
+
+
+int ChangeCmp(const void* change1, const void* change2) {
+	if (((struct Change*)change1)->position < ((struct Change*)change2)->position) {
+		return -1;
+	} else 	if (((struct Change*)change1)->position == ((struct Change*)change2)->position) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+
+int FileSize(FILE* file) {
+	assert(file != NULL);
+
+	int backupPos = ftell(file);
+
+	fseek(file, 0, SEEK_END);
+	int res = ftell(file);
+	fseek(file, backupPos, SEEK_SET);
+
+	return res;
+}
+
+
+void PrintStringTimes(int count, char str[]) {
+	for(int i = 0; i < count; ++i) {
+		printf("%s", str);
+	}
+}
+
+
+void GetCrackFName(char origFName[], char res[], int bufSize) {
+	assert(origFName != NULL);
+
+	const char postfix[] = "_cracked";
+	assert(strlen(origFName) + sizeof(postfix) <= bufSize);
+
+	char* dot = strchr(origFName, '.');
+	if (dot == NULL) {
+		dot = &origFName[strlen(origFName)];
+	}
+	int origFNameLen = dot - origFName;
+
+	strncpy(res, origFName, origFNameLen);
+	sprintf(res + origFNameLen, "%s%s", postfix, dot);
+
+	assert(strlen(res) == strlen(origFName) + sizeof(postfix) - 1);
+}
+
+
+void CrackFile(FILE* origFile, FILE* crackFile) {
+	assert(origFile != NULL);
+	assert(crackFile != NULL);
+
+	const int progressBarLen = 80;
+
+	int NChanges = sizeof(changes) / sizeof(struct Change);
+	qsort(changes, NChanges, sizeof(struct Change), ChangeCmp);
+		
+	int origFSize = FileSize(origFile);
+	char* buf = (char*)calloc(origFSize + 1, sizeof(char));
+
+	int curPos = 0;
+	int curChangePos = 0;
+	int printWarning = 0;
+	int progressStep = progressBarLen / NChanges;
+
+	PrintStringTimes(progressStep * (NChanges + 1), "▒");
+	printf("\r");
+
+	for (int i = 0; i <= NChanges; ++i) {
+
+		if (i == NChanges) {
+			curChangePos = origFSize;
+		} else {
+			curChangePos = changes[i].position;
+		}
+		assert(curPos <= curChangePos);
+
+		if (curChangePos > origFSize) {
+			fprintf(stderr, "\nError cracking file, check the input file\n");
+			return;
+		}
+		if (fread(buf, sizeof(char), curChangePos - curPos, origFile) != curChangePos - curPos) {
+			fprintf(stderr, "\nError reading original file\n");
+			return;
+		}
+		if(fwrite(buf, sizeof(char), curChangePos - curPos, crackFile) != curChangePos - curPos) {
+			fprintf(stderr, "\nError writing cracked file\n");
+			return;
+		}
+
+		if (i != NChanges) {
+			char curCh = fgetc(origFile);
+			if (curCh != changes[i].origVal) {
+				printWarning = 1;
+			}
+			fputc(changes[i].newVal, crackFile);
+		}
+
+		curPos = curChangePos + 1;
+
+		PrintStringTimes(progressStep, "█");
+		//fflush(stdout);
+		//usleep(500000);
+	}
+	printf("\n");
+
+	if (printWarning) {
+		printf("Warning! Input file is not the required program!\n"
+		       "An attempt to crack the file will still be made but it will probably not work.\n");
+	}
+
+	free(buf);
+}
+
+
+int main() {
+	char crackFName[150] = "";
+	char origFName[100] = "";
+	const char imageBuf[] = "\n\
+  ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────┐\n\
+  │ ███╗   ███╗ ██╗   ██╗ ██████╗  ███╗   ███╗ ██╗   ██╗ ██████╗   ██████╗ ██████╗   █████╗   ██████╗ ██╗  ██╗ │\n\
+  │ ████╗ ████║ ██║   ██║ ██╔══██╗ ████╗ ████║ ██║   ██║ ██╔══██╗ ██╔════╝ ██╔══██╗ ██╔══██╗ ██╔════╝ ██║ ██╔╝ │\n\
+  │ ██╔████╔██║ ██║   ██║ ██████╔╝ ██╔████╔██║ ██║   ██║ ██████╔╝ ██║      ██████╔╝ ███████║ ██║      █████╔╝  │\n\
+  │ ██║╚██╔╝██║ ██║   ██║ ██╔══██╗ ██║╚██╔╝██║ ██║   ██║ ██╔══██╗ ██║      ██╔══██╗ ██╔══██║ ██║      ██╔═██╗  │\n\
+  │ ██║ ╚═╝ ██║ ╚██████╔╝ ██║  ██║ ██║ ╚═╝ ██║ ╚██████╔╝ ██║  ██║ ╚██████╗ ██║  ██║ ██║  ██║ ╚██████╗ ██║  ██╗ │\n\
+  │ ╚═╝     ╚═╝  ╚═════╝  ╚═╝  ╚═╝ ╚═╝     ╚═╝  ╚═════╝  ╚═╝  ╚═╝  ╚═════╝ ╚═╝  ╚═╝ ╚═╝  ╚═╝  ╚═════╝ ╚═╝  ╚═╝ │\n\
+  └────────────────────────────────────────────────────────────────────────────────────────────────────────────┘\n\
+  \n";
+	printf("%s", imageBuf);
+	
+	printf("Let's crack a program! Enter file name to crack: ");
+	scanf("%99s%*c", origFName);
+
+	FILE* origFile = fopen(origFName, "rb");
+	if (origFile == NULL) {
+		fprintf(stderr, "Error opening original file\n");
+		getchar();
+		return 1;
+	}
+
+	GetCrackFName(origFName, crackFName, sizeof(crackFName));
+	FILE* crackFile = fopen(crackFName, "wb");
+	if (crackFile == NULL) {
+		fprintf(stderr, "Error creating cracked file\n");
+		getchar();
+		return 1;
+	}
+
+	printf("Cracking...\e[?25l\n");
+	CrackFile(origFile, crackFile);
+	printf("Cracked successfully\e[?25h\n");
+
+	fclose(origFile);
+	fclose(crackFile);
+	getchar();
+	return 0;
+}
---
+++ Printf/printf.asm
@@ -0,0 +1,324 @@
+global _start
+
+STDOUT		equ 1
+WRITE_CALL	equ 1
+
+
+section .text
+; Writes a character to console
+; Input:	ah - character
+; Destroy:	rax, rcx, rdx, rdi, rsi, r8-r11
+Putc:
+	mov [outCharacter], ah
+	
+	mov rax, WRITE_CALL
+	mov rdi, STDOUT
+	mov rsi, outCharacter
+	mov rdx, 1
+	syscall
+	
+	ret
+
+section .data
+outCharacter db 0
+
+
+section .text
+; Calculates the length of string
+; Input:	rdi - string address
+; Output:	rax - length
+; Destroy:      rdi, rcx, rax
+Strlen:
+        mov al, 0
+        mov rcx, 0ffffffffffffffffH
+
+        repne scasb
+
+        mov rax, 0fffffffffffffffeH
+        sub rax, rcx
+
+        ret
+
+
+; Writes a string to console (must be null-terminated)
+; Input:	rdi - string address
+; Destroy:	rax, rcx, rdx, rdi, rsi, r8-r11
+Puts:
+	mov rsi, rdi	; rdi backup
+	call Strlen	; In rax is the string length now
+	
+	mov rdx, rax
+	mov rax, WRITE_CALL
+	mov rdi, STDOUT
+	;rsi already set
+	syscall
+
+	ret
+
+
+; Converts integer to string in givan base
+; Input:	eax - number
+; 		ecx - base
+; Output:	rdi - address to buffer with required number (null-terminated)
+; Destroy:	previos number in buffer, eax, edx
+IntToBase:
+	NUMBUF_SIZE	equ 33D
+	
+	mov rdi, numBuf + NUMBUF_SIZE - 1
+	mov byte [rdi], 0
+	dec rdi
+	.convert_loop:
+		xor edx, edx	; mov edx, 0
+		div ecx		; in edx is the quotient
+		add dl, '0'	; we "convert" edx to ascii
+		
+		cmp edx, '9'
+		jbe .end_if_letter
+			add dl, -'0' - 10D + 'a'
+		.end_if_letter:
+
+		mov [rdi], dl	; saving current digit to buffer
+		dec rdi
+		
+		cmp eax, 0
+		jne .convert_loop
+
+	inc rdi
+	ret
+
+section .data
+numBuf times NUMBUF_SIZE db 0
+
+
+section .text
+; Writes a decimal integer to console
+; Input:	rdi - address of integer
+; Destroy:	rax, rcx, rdx, rdi, rsi, r8-r11
+Putd:
+	mov eax, [rdi]
+	cmp eax, 0
+	jge .end_if_negative
+		not eax
+		inc eax
+
+		push rax	; backup eax
+		mov ah, '-'
+		call Putc
+		pop rax		; restore eax
+	.end_if_negative:
+
+	mov ecx, 10D
+	call IntToBase	; in rdi is the required string now
+
+	call Puts
+
+	ret
+
+
+; Writes hex number to console
+; Input:	rdi - address of number
+; Destroy:	rax, rcx, rdx, rdi, rsi, r8-r11
+Putx:
+	mov eax, [rdi]
+	mov ecx, 16D
+	call IntToBase	; in rdi is the required string now
+
+	call Puts
+
+	ret
+
+
+; Writes binary number to console
+; Input:	rdi - address of number
+; Destroy:	rax, rcx, rdx, rdi, rsi, r8-r11
+Putb:
+	mov eax, [rdi]
+	mov ecx, 2
+	call IntToBase	; in rdi is the required string now
+
+	call Puts
+
+	ret
+
+
+; Prints a string based on format string and parameters to comsole
+; Input:	In stack are the format string and parameters (at the top is
+; 			the format string, first parameter is in the secomd place)
+; Destroy:	rax, rbx, rcx, rdx, rdi, rsi, r8-r11
+Printf:
+	pop rbx
+	
+	pop rcx		; Current character address
+	.print_loop:
+		cmp byte [rcx], '%'
+		jne .if_not_percent
+			inc rcx
+			mov ah, [rcx]	; format cpecifier
+			
+			; Switch format specifier
+			xor rdx, rdx	; table offset
+
+			cmp ah, 'c'
+			je .jump_rdx
+			inc rdx
+			
+			cmp ah, 's'
+			je .jump_rdx
+			inc rdx
+
+			cmp ah, 'd'
+			je .jump_rdx
+			inc rdx
+			
+			cmp ah, 'x'
+			je .jump_rdx
+			inc rdx
+			
+			cmp ah, 'b'
+			je .jump_rdx
+			inc rdx
+
+			cmp ah, '%'
+			je .jump_rdx
+			inc rdx
+
+			cmp ah, 0
+			je .jump_rdx
+			inc rdx
+			jmp .end_switch_format
+			
+			.jump_rdx:
+			jmp qword [.jump_table + rdx * 8]
+
+			.case_format_c:
+				pop rdi		; get parameter
+				push rcx	; rcx backup
+				mov ah, [rdi]
+				call Putc
+				pop rcx		; restore rcx
+
+				jmp .end_switch_format
+			.case_format_s:
+				pop rdi		; get parameter
+				push rcx	; rcx backup
+				call Puts
+				pop rcx		; restore rcx
+
+				jmp .end_switch_format
+			.case_format_d:
+				pop rdi		; get parameter
+				push rcx	; rcx backup
+				call Putd
+				pop rcx		; restore rcx
+
+				jmp .end_switch_format
+			.case_format_x:
+				pop rdi		; get parameter
+				push rcx	; rcx backup
+				call Putx
+				pop rcx		; restore rcx
+
+				jmp .end_switch_format
+			.case_format_b:
+				pop rdi		; get parameter
+				push rcx	; rcx backup
+				call Putb
+				pop rcx		; restore rcx
+
+				jmp .end_switch_format
+			.case_format_percent:
+				push rcx	; rcx backup
+				; ah already set
+				call Putc
+				pop rcx		; restore rcx
+
+				jmp .end_switch_format
+			.case_format_null:
+				jmp .return	; end of string
+			
+			.jump_table:	dq .case_format_c,
+							dq .case_format_s,
+							dq .case_format_d,
+							dq .case_format_x,
+							dq .case_format_b,
+							dq .case_format_percent,
+							dq .case_format_null
+			
+			.end_switch_format:
+
+			inc rcx
+			
+			jmp .end_if_PercSlash
+		.if_not_percent:
+			mov ah, [rcx]
+			cmp ah, '\'
+
+			jne .skip_bslash
+			inc rcx
+			mov ah, [rcx]
+
+			; Switch escape character
+			cmp ah, 'n'
+			je .case_esc_n
+
+			cmp ah, 't'
+			je .case_esc_t
+
+			cmp ah, 0
+			je .case_esc_null
+			jmp .end_switch_esc	; ah will be already set for mirroring
+
+			.case_esc_n:
+				mov ah, 0aH
+				jmp .end_switch_esc
+			.case_esc_t:
+				mov ah, 9
+				jmp .end_switch_esc
+			.case_esc_null:
+				jmp .return
+			.end_switch_esc:
+
+			
+			.skip_bslash:
+			push rcx	; rcx backup
+			call Putc
+			pop rcx		; restore rcx
+			
+			inc rcx
+		.end_if_PercSlash:
+		
+		cmp byte [rcx], 0
+		jne .print_loop
+	
+	.return:
+	push rbx
+	ret
+
+
+
+_start:
+	push number1
+	push string2
+	push string1
+	push character
+	push formatString
+
+	call Printf
+
+	mov rax, 03cH
+	mov rdi, 0
+	syscall
+
+section .data
+;formatString db "\n\tTest %s %c %%%s%% %d %b %x \\\a\\ \n\n", 0
+;character db 'h'
+;string db "string test", 0
+;number1 dd -8324756D
+;number2 dd 0bef8H
+
+formatString db "\n\t%c %s %s %b\n\n", 0
+character db 'I'
+string1 db "love", 0
+string2 db "EDA", 0
+number1 dd 124D
+
